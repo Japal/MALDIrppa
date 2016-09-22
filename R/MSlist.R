a2fe86a1cc980aec17cf52126d2ba970aa043e91
Japal <javier.palarea@bioss.ac.uk>
2016-09-22 17:26:50 +0100

    Update

--- R/MSlist.R
+++ R/MSlist.R
@@ -1,8 +1,4 @@
-MSlist <-
-function(mz,I){
-  # Generate list of MassSpectrum objects for MALDIquant (sampleIDs as list element names)
-  # mz: common m/z vector
-  # I: matrix with spectra intensities by columns (sample IDs included as column names)
+MSlist <- function(mz,I){
 	
   d <- list()
   for (i in 1:ncol(I)){
--- R/MSlist2.R
+++ R/MSlist2.R
@@ -1,8 +1,4 @@
-MSlist2 <-
-function(mz,I){
-  # Generate list of MassPeaks objects for MALDIquant (sampleIDs as list element names)
-  # mz: common m/z vector
-  # I: matrix with spectra intensities by columns (sample IDs included as column names)
+MSlist2 <- function(mz,I){
    
   d <- list()
   for (i in 1:ncol(I)){
