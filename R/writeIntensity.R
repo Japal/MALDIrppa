7c1191235019b98e287f1b154376fd8f95ac2cc7
Japal <javier.palarea@bioss.ac.uk>
2017-02-06 09:34:53 +0000

    FASTA format added

--- R/writeIntensity.R
+++ R/writeIntensity.R
@@ -1,4 +1,4 @@
-writeIntensity <- function(x,filename="intMatrix",format=c("R","csv","NEXUS"),binary=FALSE, labels=NULL, weights=NULL,...){
+writeIntensity <- function(x,filename="intMatrix",format=c("R","csv","NEXUS","FASTA"),binary=FALSE, labels=NULL, weights=NULL,...){
   
   if (!is.matrix(x) && !is.data.frame(x)) stop("x must be matrix or data.frame.")
   
@@ -85,12 +85,6 @@ writeIntensity <- function(x,filename="intMatrix",format=c("R","csv","NEXUS"),bi
            date(), "]\n")
       NCHAR <- paste("NCHAR=", nchars, sep = "")
       NTAX <- paste("NTAX=", ntax, sep = "")
-      if (format == "dna") {
-        DATATYPE <- "DATATYPE=DNA"
-      }
-      if (format == "protein") {
-        DATATYPE <- "DATATYPE=PROTEIN"
-      }
       if (format == "standard") {
         DATATYPE <- "DATATYPE=STANDARD"
       }
@@ -177,15 +171,66 @@ writeIntensity <- function(x,filename="intMatrix",format=c("R","csv","NEXUS"),bi
       close(zz)
     }
   
+  exportFasta <- function (x, file, append = FALSE, nbcol = 6, colw = 10, indent = NULL, ...) 
+  {
+    
+    N <- dim(x)
+    S <- N[2]
+    N <- N[1]
+    xx <- vector("list", N)
+    for (i in 1:N) xx[[i]] <- x[i, ]
+    names(xx) <- rownames(x)
+    x <- xx
+    rm(xx)
+    
+    if (!is.null(labels))
+      if (is.data.frame(labels)) labels <- labels[,1] # Convert to vector
+      names(x) <- labels
+    if (is.null(names(x))) 
+      names(x) <- as.character(1:N)
+    if (is.null(indent)) 
+      indent <- 0
+    if (is.numeric(indent)) 
+      indent <- paste(rep(" ", indent), collapse = "")
+    
+    zz <- if (append) 
+      file(file, "a")
+    else file(file, "w")
+    on.exit(close(zz))
+    
+    for (i in 1:N) {
+      cat(">", names(x)[i], file = zz, sep = "")
+      cat("\n", file = zz)
+      X <- paste(x[[i]], collapse = "")
+      S <- length(x[[i]])
+      totalcol <- ceiling(S/colw)
+      if (nbcol < 0) nbcol <- totalcol
+      nb.lines <- ceiling(totalcol/nbcol)
+      SEQ <- character(totalcol)
+      for (j in 1:totalcol) SEQ[j] <- substr(X, 1 + (j - 
+                                                       1) * colw, colw + (j - 1) * colw)
+      for (k in 1:nb.lines) {
+        endsel <- if (k == nb.lines) length(SEQ) else nbcol + 
+          (k - 1) * nbcol
+        cat(indent, file = zz)
+        cat(SEQ[(1 + (k - 1) * nbcol):endsel], sep = "", 
+            file = zz)
+        cat("\n", file = zz)
+      }
+    }
+  }
+  
   colnames(x) <- as.character(round(as.numeric(colnames(x)),4))
   x[is.na(x)] <- 0 # Use 0 to indicate no peak
   if ((binary==TRUE) | (format=="NEXUS")) {x <- (x!=0)*1}
+  if ((binary==TRUE) | (format=="FASTA")) {x <- (x!=0)*1}
   
   assign(nam,x)   
   
   switch(format,
          R=save(list=eval(nam),file=paste(filename,".Rdata",sep=""), ...),
          csv=write.table(x,file=paste(filename,".csv",sep=""),sep=",",row.names=FALSE, ...),
-         NEXUS=exportNexus(x,file=paste(filename,".nex",sep=""), ...)
+         NEXUS=exportNexus(x,file=paste(filename,".nex",sep=""), ...),
+         FASTA=exportFasta(x,file=paste(filename,".fas",sep=""), ...)
          )
 }
