a48beb301b3c5adbb3bf45c04924535b478d50d6
Japal <javier.palarea@bioss.ac.uk>
2020-05-28 19:25:27 +0100

    Update

--- R/wavSmoothing.R
+++ R/wavSmoothing.R
@@ -1,10 +1,17 @@
-wavSmoothing <- function(x, thresh.scale = 2.5, ...){
+wavSmoothing <- function(x, method=c("Wavelet","SavitzkyGolay","MovingAverage"), thresh.scale = 2.5, ...){
 
   if (!isMassSpectrumList(x)) {
     stop("x must be a list of MassSpectrum class objects")
   }
   
-  x <- lapply(x, FUN = transfIntensity, fun = function(x) wavShrink(x, thresh.scale = thresh.scale, ...))
+  method <- match.arg(method)
+  
+  if (method=="Wavelet"){
+    x <- lapply(x, FUN = transfIntensity, fun = function(x) wmtsa::wavShrink(x, thresh.scale = thresh.scale, ...))
+  }
+  
+  if (method=="SavitzkyGolay") {x <- MALDIquant::smoothIntensity(x, method="SavitzkyGolay", ...)}
+  if (method=="MovingAverage") {x <- MALDIquant::smoothIntensity(x, method="MovingAverage", ...)}
 
   return(x)  
 
